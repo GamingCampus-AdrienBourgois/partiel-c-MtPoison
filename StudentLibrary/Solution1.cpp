1a70104ea9d917e67aaf84a27b8caeb95a81cf03
Quentin <quentinleforestier0@gmail.com>
2024-01-19 13:44:23 +0100

    test4

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -14,7 +14,7 @@ float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, Temper
 
 	if (_from == TemperatureUnits::KELVIN && _to == TemperatureUnits::FAHRENHEIT)
 	{
-			Nouvelletemperature = (_value - 273.15) *9.0 / 5.0 + 32.0;
+			Nouvelletemperature = (_value - 273.15) * 9.0 / 5.0 + 32.0;
 	}
 	if (_from == TemperatureUnits::CELSIUS && _to == TemperatureUnits::KELVIN)
 	{
@@ -23,17 +23,21 @@ float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, Temper
 
 	if (_from == TemperatureUnits::CELSIUS && _to == TemperatureUnits::FAHRENHEIT)
 	{
-		Nouvelletemperature = (_value - 9.0 / 5.0) + 32.0 ;
+		Nouvelletemperature = _value * 9.0 / 5.0 + 32.0 ;
 	}
 
 	if (_from == TemperatureUnits::FAHRENHEIT && _to == TemperatureUnits::CELSIUS)
 	{
-		Nouvelletemperature = _value - 273.15;
+		Nouvelletemperature = (_value - 32.0) * 5.0/9.0;
 	}
 
 	if (_from == TemperatureUnits::FAHRENHEIT && _to == TemperatureUnits::KELVIN)
 	{
 		Nouvelletemperature = (_value - 32.0) * 5.0 / 9.0 + 273.15;
+	}
+	if (_from == TemperatureUnits::CELSIUS && _to == TemperatureUnits::CELSIUS)
+	{
+		Nouvelletemperature = _value;
 	}
 	return Nouvelletemperature;
 }
