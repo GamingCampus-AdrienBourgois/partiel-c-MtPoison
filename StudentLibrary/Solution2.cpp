1e13f434521dc71be1d46576e386b649ec2b46f6
Quentin <quentinleforestier0@gmail.com>
2024-01-19 14:23:10 +0100

    aller

--- StudentLibrary/Solution2.cpp
+++ StudentLibrary/Solution2.cpp
@@ -1,5 +1,8 @@
 #include "Solution2.h"
-
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 #include <ostream>
 
 // Don't forget to enable the exercise in the SudentConfiguration.h file !
@@ -8,7 +11,44 @@
 
 float Solution2::GetBalance(const std::string& accountName)
 {
-	return -1.0f;
+    std::string nomFichier = "x64\\Debug\\BankAccount"+ accountName +".txt";
+
+    std::ifstream fichier(nomFichier);
+
+    if (!fichier.is_open()) 
+    {
+        std::cout << "Impossible d'ouvrir le fichier : " << nomFichier << std::endl;
+        return 1; 
+    }
+
+    std::string ligne;
+    double solde = 0.0;
+
+    while (std::getline(fichier, ligne))
+    {
+        std::istringstream iss(ligne);
+        std::string operation;
+        double montant;
+
+        if (iss >> operation >> montant)
+        {
+            if (operation == "deposit")
+            {
+                solde += montant;
+            }
+            else if (operation == "withdraw")
+            {
+                solde -= montant;
+            }
+            else {
+                std::cout << "Opération non reconnue : " << operation << std::endl;
+            }
+        }
+        else {
+            std::cout << "Erreur de lecture de la ligne : " << ligne << std::endl;
+        }
+    }
+	return solde;
 }
 
 #endif
